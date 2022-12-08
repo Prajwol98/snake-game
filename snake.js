84556384d99e0948f3a7f28544f9fe84b023d6ea
Prajwol Mainali <prajwolmainali96@gmail.com>
2022-12-08 11:18:41 +0545

    snake game

---
+++ snake.js
@@ -0,0 +1,43 @@
+// game const and variables
+let direction={x:0,y:0};
+const foodSound=new Audio('food.mp3');
+const gameOverSound=new Audio('gameover.mp3');
+const moveSound=new Audio('move.mp3');
+const musicSound=new Audio('music.mp3');
+let speed=2;
+let lastPaintTime=0;
+let snakeArray=[{
+    x:13,y:15
+}]
+
+// function
+function main(cTime){
+    window.requestAnimationFrame(main);
+    console.log(cTime)
+    if((cTime-lastPaintTime)/1000<1/speed);{
+        return;
+    }
+    lastPaintTime=cTime;
+    gameEngine();
+
+}
+function gameEngine(){
+    // part 1=updating the snake food amd array 
+
+    // part 2=display the snake and food
+    board.innerHTMl='';
+    snakeArray.forEach((e,index)=>{
+        snakeElement=document.createElement('div');
+        snakeElement.style.gridRowStart=e.y;
+        snakeElement.style.gridColumnStart=e.x;
+    })
+}
+
+
+
+
+
+
+// main logic start from here
+window.requestAnimationFrame(main);
+
